c259d99adab213bfba5b9ee7c2fca412edff4a11
msh <alstngusqkqh@gmail.com>
2025-10-25 14:52:53 +0900

    로그인 기능 구현

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, TextAreaField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm) :
-    std_num = StringField('학번', validators=[DataRequired()])
-    password = PasswordField('비밀번호', validators=[DataRequired()])
+    std_num = StringField('학번', validators=[DataRequired("학번을 입력하세요.")])
+    password = PasswordField('비밀번호', validators=[DataRequired("비밀번호를 입력하세요.")])
--- app/views/login_views.py
+++ app/views/login_views.py
@@ -1,7 +1,41 @@
-from flask import Blueprint, url_for, render_template
+from flask import Blueprint, url_for, render_template, request, session, flash, redirect, g
+from app.models import Admins
+from app.forms import LoginForm
+import bcrypt
 
 bp = Blueprint('login', __name__, url_prefix='/login')
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def login() :
-    return render_template('login_page.html')
+    form = LoginForm()
+    if request.method == 'POST' and form.validate_on_submit() :
+        error = None
+        admin = Admins.query.filter_by(std_num=form.std_num.data).first()
+
+        if not admin :
+            error = "존재하지 않는 학번입니다."
+        
+        elif not bcrypt.checkpw(form.password.data.encode('utf-8'), admin.password.encode('utf-8')) :
+            error = "비밀번호가 올바르지 않습니다."
+        
+        if error is None :
+            session.clear()
+            session['admin_id'] = admin.id
+            return redirect(url_for('main.main'))
+        
+        flash(error)
+
+    return render_template('login_page.html', form=form)
+
+@bp.before_app_request
+def load_logged_in_admin() :
+    admin_id = session.get('admin_id')
+    if admin_id is None :
+        g.admin = None
+    else :
+        g.admin = Admins.query.get(admin_id)
+
+@bp.route('/logout/')
+def logout() :
+    session.clear()
+    return redirect(url_for('main.main'))
